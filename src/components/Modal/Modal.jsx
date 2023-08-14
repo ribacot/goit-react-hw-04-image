411cca0c7922d919e70c23d8336036701b4cf776
ribacot <remontdiskov1979@gmail.com>
2023-08-14 18:53:19 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,7 @@
-import { useState, useEffect } from 'react';
+import {useEffect } from 'react';
 import css from './Modal.module.css';
 
 export default function Modal({ children, onClick }) {
-  useEffect(() => {
-    window.addEventListener('keydown', hendleModal);
-
-    return () => {
-      window.removeEventListener('keydown', hendleModal);
-    };
-  }, []);
 
   const hendleModal = e => {
     if (e.code === 'Escape') {
@@ -17,6 +10,15 @@ export default function Modal({ children, onClick }) {
       onClick();
     }
   };
+
+  useEffect(() => {
+    window.addEventListener('keydown', hendleModal);
+
+    return () => {
+      window.removeEventListener('keydown', hendleModal);
+    };
+  }, []);
+
 
   return (
     <div className={css.overlay} onClick={onClick}>
