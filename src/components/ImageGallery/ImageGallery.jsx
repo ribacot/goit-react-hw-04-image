082929cad6906fa1fe44cf299b0f18a8128b4f16
ribacot <remontdiskov1979@gmail.com>
2023-08-14 18:28:15 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import pixabayAPI from 'components/Sevice_Api/Pixabay_API';
 import { Circles } from 'react-loader-spinner';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
@@ -13,22 +13,11 @@ export default function ImageGallery({ searchQwery }) {
   const [page, setPage] = useState(1);
   const [visible, setVisible] = useState(false);
   const per_page = 12;
-  
-  // useEffect(() => {
-  //   setVisible(true);
-  //   if (!pictures.length) {
-  //     pixabayAPI({})
-  //       .then(resp => { setPictures(resp.hits);
-  //         if (resp.totalHits > 12) setIsloadMore(true);
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       })
-  //       .finally(setVisible(false));
-  //   }
-  // }, [pictures.length]);
 
   useEffect(() => {
+    if (!searchQwery & !pictures.length) return;
+    console.log('searchQwery', searchQwery);
+
     setVisible(true);
     pixabayAPI({
       q: searchQwery,
@@ -38,14 +27,19 @@ export default function ImageGallery({ searchQwery }) {
         (resp.hits.length === per_page) & (resp.totalHits > per_page)
           ? setIsloadMore(true)
           : setIsloadMore(false);
-        setPictures(resp.hits);
+        setPictures(prev => [...prev, ...resp.hits]);
       })
       .catch(error => console.log(error))
       .finally(setVisible(false));
   }, [searchQwery, page, per_page]);
 
-  const toggleModal = () => {
-    this.setState(prevState => ({ modalIsOpen: !prevState.modalIsOpen }));
+  useEffect(() => {
+    setPictures([]);
+    setPage(1);
+  }, [searchQwery]);
+
+  const toggleModal = e => {
+    if (e.target === e.currentTarget) setModalIsOpen(prev => !prev);
   };
 
   const hendleImage = async e => {
@@ -80,127 +74,10 @@ export default function ImageGallery({ searchQwery }) {
       </ul>
       {isloadMore && <LoadMore onClick={loadMore} />}
       {modalIsOpen && (
-        <Modal onClick={this.toggleModal}>
-          <img src={largeImage} alt="" onClick={toggleModal} />
+        <Modal onClick={toggleModal}>
+          <img src={largeImage} alt="" />
         </Modal>
       )}
     </>
   );
 }
-
-// export default class ImageGallery extends Component {
-//   state = {
-//     pictures: [],
-//     largeImage: null,
-//     modalIsOpen: false,
-//     isloadMore: false,
-//     per_page: 12,
-//     page: 1,
-//     visible: false,
-//   };
-
-//   async componentDidMount() {
-//     this.setState({ visible: true });
-//     if (!this.state.pictures.length) {
-//       try {
-//         const resp = await pixabayAPI({});
-//         if (resp.totalHits > 12) this.setState({ isloadMore: true });
-
-//         this.setState({ pictures: resp.hits });
-//       } catch (error) {
-//         console.log(error);
-//       } finally {
-//         this.setState({ visible: false });
-//       }
-//     }
-//   }
-//   async componentDidUpdate(prevProps, prevState) {
-//     const { searchQwery } = this.props;
-//     const { per_page, page } = this.state;
-
-//     if (page !== prevState.page || searchQwery !== prevProps.searchQwery) {
-//       if (searchQwery !== prevProps.searchQwery) {
-//         this.setState({ pictures: [], page: 1 });
-//         try {
-//           const resp = await pixabayAPI({
-//             q: searchQwery,
-//             page,
-//           });
-//           (resp.hits.length === per_page) & (resp.totalHits > per_page)
-//             ? this.setState({ isloadMore: true })
-//             : this.setState({ isloadMore: false });
-//           this.setState({ pictures: resp.hits });
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       } else {
-//         try {
-//           const resp = await pixabayAPI({ page, q: searchQwery });
-//           console.log(resp.hits.length);
-//           (resp.hits.length === per_page) & (resp.totalHits > per_page)
-//             ? this.setState({ isloadMore: true })
-//             : this.setState({ isloadMore: false });
-
-//           this.setState(prevState => {
-//             return {
-//               pictures: [...prevState.pictures, ...resp.hits],
-//               total: resp.total,
-//             };
-//           });
-//           console.log(resp);
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       }
-//     }
-//   }
-//   toggleModal = () => {
-//     this.setState(prevState => ({ modalIsOpen: !prevState.modalIsOpen }));
-//   };
-
-//   hendleImage = async e => {
-//     const { id } = e.target;
-
-//     try {
-//       const resp = await pixabayAPI({ id });
-//       const largeImage = resp.hits[0].largeImageURL;
-//       this.setState({ largeImage, modalIsOpen: true });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => {
-//       return { page: prevState.page + 1 };
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <Circles visible={this.state.visible} />
-//         <ul className={css.ImageGallery}>
-//           {this.state.pictures.map(({ id, webformatURL }) => (
-//             <ImageGalleryItem
-//               key={id}
-//               id={id}
-//               webformatURL={webformatURL}
-//               onClick={this.hendleImage}
-//             />
-//           ))}
-//         </ul>
-//         {this.state.isloadMore && <LoadMore onClick={this.loadMore} />}
-//         {this.state.modalIsOpen && (
-//           <Modal onClick={this.toggleModal}>
-//             <img
-//               src={this.state.largeImage}
-//               alt=""
-//               onClick={this.toggleModal}
-//             />
-//           </Modal>
-//         )}
-//       </>
-//     );
-//   }
-// }
